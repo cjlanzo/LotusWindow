2d4c1a45a3cc5c3feb4dc359745ec1ceab931455
Chris Lanzo <LanzoC@b2b.regn.net>
2019-12-04 16:43:16 -0500

    updated how version affects execution

--- EventHandlers.lua
+++ EventHandlers.lua
@@ -16,33 +16,42 @@ function HandleChatMsgLoot(args)
 end
 
 function HandleChatMsgAddon(args)
+    -- might need to move this to a separate function if you can't nest
+    local function GetVersionComponents(version)
+        return string.match(version, "(%d+).(%d+).(%d+)")
+    end
+
+    local function ConvertVersionToNumber(version)
+        return tonumber(string.format("%d%d%d", GetVersionComponents(version)))
+    end
+
     local prefix, payload, _, sender = args
-    local addonPrefix, version = string.match(prefix, "([%a-]+):([%d.]+)")
-    local major, minor, patch = string.match(version, "(%d+).(%d+).(%d+)")
-    local currentMajor, currentMinor, currentPatch = string.match(ADDON_VERSION, "(%d+).(%d+).(%d+)")
+    local addonPrefix, incomingVersion = string.match(prefix, "([%a-]+):([%d.]+)")
+    local major, minor, patch = GetVersionComponents(incomingVersion)
+    local currentMajor, currentMinor, currentPatch = GetVersionComponents(ADDON_VERSION)
 
-    if major > currentMajor then
-        print("Incoming addon message uses an incompatible version of LotusWindow")
-    else 
-        if minor > currentMinor then
-            print("An update for LotusWindow is available")
-        end
+    if ConvertVersionToNumber(incomingVersion) > ConvertVersionToNumber(ADDON_VERSION) then
+        print("An update for LotusWindow is available")
+    end
 
-        if addonPrefix == MAIN_PREFIX then
+    if addonPrefix == MAIN_PREFIX then
+        if major > currentMajor then
+            print("Incoming addon message uses an incompatible version of LotusWindow")
+        else 
             local zone, timer, updated = string.match(payload, "(%a+%s*%a*):([%d-]+):(%d+)")
             
             if updated > lastUpdated[zone] then
                 timers[zone] = timer
                 lastUpdated[zone] = updated
-    
+
                 DisplayTimerForZone(zone)
                 print("Lotus timer updated by "..sender)
             end
-        elseif addonPrefix == REQUEST_PREFIX then
-            local zone = payload
-
-            SendUpdate(zone)
-            print("Update for "..zone.." requested by "..sender)
         end
+    elseif addonPrefix == REQUEST_PREFIX then
+        local zone = payload
+
+        SendUpdate(zone)
+        print("Update for "..zone.." requested by "..sender)
     end
 end
